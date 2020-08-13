cf08381e7121dfec02de30a401c824208b20f796
Gerard Richardson <gerardr@spotify.com>
2020-08-13 00:07:00 -0400

    api cors

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,15 @@
-console.log(0002)
+// get quote
+async function getQuote() {
+  const proxyURL = 'https://quiet-fjord-23740.herokuapp.com/'
+  const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&formnat=json'
+
+  try {
+    const response = await fetch(proxyURL + apiURL)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getQuote()
