40101088ceda6012893bd71f43c37ea1a395406b
Andre <g.andrerichards@gmail.com>
2021-07-21 23:43:07 -0400

    add api url to fetch quotes

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,15 +1,25 @@
-// get quote
-async function getQuote() {
+// to persist
+let apiQuotes = []
+
+// show new quote
+const newQuote = () => {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  console.log(quote)
+}
+
+// fetch from the api
+async function getQuotes() {
   const proxyURL = 'https://quiet-fjord-23740.herokuapp.com/'
-  const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&formnat=json'
+  // const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&formnat=json'
+  const apiURL = `https://type.fit/api/quotes`
 
   try {
     const response = await fetch(proxyURL + apiURL)
-    const data = await response.json()
-    console.log(data)
+    apiQuotes = await response.json()
+    newQuote()
   } catch (error) {
     console.log(error)
   }
 }
 
-getQuote()
+getQuotes()
