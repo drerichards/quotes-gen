0657014359686ddea0fc9a00098849d702bd6e07
Andre <g.andrerichards@gmail.com>
2021-09-26 01:32:17 -0400

    add loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,23 +3,42 @@ const quoteText = document.getElementById('quote-text')
 const quoteAuthor = document.getElementById('quote-author')
 const quoteButton = document.getElementById('new-button')
 const twitterButton = document.getElementById('twitter-button')
+const loader = document.getElementById('loader')
 
+
+// show loading
+function loading() {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+// hide loading
+function complete() {
+  loader.hidden = true
+  quoteContainer.hidden = false
+}
 
 // to persist
 let apiQuotes = []
 
 // show new quote
 const newQuote = () => {
+  loading()
+  
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   quote.text.length > 50 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote')
+  
   quoteText.textContent = quote.text
   quoteAuthor.textContent = !quote.author ? 'Unknown' : quote.author
+  
+  complete()
 }
 
 quoteButton.addEventListener('click', newQuote)
 
 // fetch from the api
 async function getQuotes() {
+  loading()
   const proxyURL = 'https://quiet-fjord-23740.herokuapp.com/'
   // const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&formnat=json'
   const apiURL = `https://type.fit/api/quotes`
@@ -33,4 +52,6 @@ async function getQuotes() {
   }
 }
 
+// on load
 getQuotes()
+// loading()
